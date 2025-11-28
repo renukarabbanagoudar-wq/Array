530a3f14c0f9f722dc8e10467bad983a6123ec3b
renukarabbanagoudar-wq <renuka.rabbanagoudar@gmail.com>
2025-11-28 09:34:37 +0530

    save

---
+++ scores.py
@@ -0,0 +1,18 @@
+import sys
+if len(sys.argv) > 1:
+    scores = list(map(float, sys.argv[1:]))
+    print("User provided scores:")
+else:
+    # Default values
+    scores = [12, 24, 36, 42, 60]
+    print("No input given - using default scores:")
+
+script_name = sys.argv[0]
+
+total = sum(scores)
+average = total / len(scores)
+
+print("Script Name:", script_name)
+print("Scores:", scores)
+print("Sum of Scores:", total)
+print("Average Score:", average)
